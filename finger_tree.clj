598797eaf6b7f67f8b8723124ecbd167d7ffd00a
Chouser <chouser@n01se.net>
2009-08-26 20:17:15 -0400

    Make room in trees and nodes for cache-fns

--- finger_tree.clj
+++ finger_tree.clj
@@ -2,77 +2,92 @@
 
 (use 'clojure.test)
 
+(defn node [cache-fns & xs]
+  (vec xs))
 
-(defn finger-tree [l m r]
-  [l m r])
 
-(defn single [x]
-  [[x] nil []])
+(defn deep-tree [l m r & [cache-fns]]
+  [l m r cache-fns])
+
+(defn single [[_ _ _ cache-fns] x]
+  [(node cache-fns x) nil [] cache-fns])
 
 (defn single? [l m r]
   (and (< (count l) 2) (empty? m) (empty? r)))
 
+(defn ft-empty? [[l m r]]
+  (and (empty? l) (empty? m) (empty? r)))
+
 
 (defn ft-seq [t]
-  (when-let [[l m r] t]
-    (lazy-cat l (apply concat (ft-seq m)) r)))
+  (when-not (ft-empty? t)
+    (let [[l m r] t]
+      (lazy-cat l (apply concat (ft-seq m)) r))))
 
 (defn ft-rseq [t]
-  (when-let [[l m r] t]
-    (lazy-cat (reverse r)
-              (mapcat reverse (ft-rseq m))
-              (reverse l))))
+  (when-not (ft-empty? t)
+    (let [[l m r] t]
+      (lazy-cat (reverse r)
+                (mapcat reverse (ft-rseq m))
+                (reverse l)))))
 
 (defn conjl [t a]
-  (if-let [[l m r] t]
-    (if (< (count l) 4)
-      (if (single? l m r)
-        (finger-tree [a] nil l)
-        (finger-tree (vec (cons a l)) m r))
-      (let [[b c d e] l]
-        (finger-tree [a b] (conjl m [c d e]) r)))
-    (single a)))
+  (if-not (ft-empty? t)
+    (let [[l m r cache-fns] t]
+      (if (< (count l) 4)
+        (if (single? l m r)
+          (deep-tree (node cache-fns a) nil l)
+          (deep-tree (apply node cache-fns (cons a l)) m r))
+        (let [[b c d e] l]
+          (deep-tree [a b] (conjl m (node cache-fns c d e)) r))))
+    (single t a)))
 
 (defn conjr [t a]
-  (if-let [[l m r] t]
-    (if (< (count r) 4)
-      (if (single? l m r)
-        (finger-tree l nil [a])
-        (finger-tree l m (conj r a)))
-      (let [[e d c b] r]
-        (finger-tree l (conjr m [e d c]) [b a])))
-    (single a)))
+  (if-not (ft-empty? t)
+    (let [[l m r cache-fns] t]
+      (if (< (count r) 4)
+        (if (single? l m r)
+          (deep-tree l nil (node cache-fns a))
+          (deep-tree l m (apply node cache-fns (conj r a))))
+        (let [[e d c b] r]
+          (deep-tree l (conjr m (node cache-fns e d c)) (node cache-fns b a)))))
+    (single t a)))
 
-(defn nodes [[a b c & xs :as s]]
+(defn finger-tree [cache-fns & xs]
+  (reduce conjr (deep-tree [] nil [] cache-fns) xs))
+
+(defn- nodes [cache-fns [a b c & xs :as s]]
   (assert (> (count s) 1))
   (condp = (count s)
-    2 [[a b]]
-    3 [[a b c]]
-    4 [[a b] [c (first xs)]]
-    (cons [a b c] (nodes xs))))
+    2 [(node cache-fns a b)]
+    3 [(node cache-fns a b c)]
+    4 [(node cache-fns a b) (node cache-fns c (first xs))]
+    (cons (node cache-fns a b c) (nodes cache-fns xs))))
 
-(defn app3 [[l1 m1 r1 :as t1] ts [l2 m2 r2 :as t2]]
+(defn- app3 [[l1 m1 r1 cache-fns :as t1] ts [l2 m2 r2 :as t2]]
   (cond
-    (empty? t1) (reduce conjl t2 (reverse ts))
-    (empty? t2) (reduce conjr t1 ts)
+    (ft-empty? t1) (reduce conjl t2 (reverse ts))
+    (ft-empty? t2) (reduce conjr t1 ts)
     (single? l1 m1 r1) (conjl (reduce conjl t2 (reverse ts)) (l1 0))
     (single? l2 m2 r2) (conjr (reduce conjr t1 ts) (l2 0))
-    :else (finger-tree l1 (app3 m1 (nodes (concat r1 ts l2)) m2) r2)))
+    :else (deep-tree l1 (app3 m1 (nodes cache-fns (concat r1 ts l2)) m2) r2
+                     cache-fns)))
 
 (defn ft-concat [t1 t2]
+  (assert (= (t1 3) (t2 3))) ; cache-fns must be the same
   (app3 t1 nil t2))
 
 (deftest Conj-Seq
   (let [len 100]
     (are [x] (= x (range len))
-      (ft-rseq (reduce conjl nil (range len)))
-      (ft-seq  (reduce conjr nil (range len))))
+      (ft-rseq (reduce conjl (finger-tree nil) (range len)))
+      (ft-seq  (reduce conjr (finger-tree nil) (range len))))
     (are [x] (= x (range (dec len) -1 -1))
-      (ft-rseq (reduce conjr nil (range len)))
-      (ft-seq  (reduce conjl nil (range len))))
+      (ft-rseq (reduce conjr (finger-tree nil) (range len)))
+      (ft-seq  (reduce conjl (finger-tree nil) (range len))))
     
     (doseq [m (range 2 7)]
-      (loop [ft nil, vc [], i (int 0)]
+      (loop [ft (finger-tree nil), vc [], i (int 0)]
         (if-not (< i 40)
           (is (= (ft-seq ft) (seq vc)))
           (if (zero? (rem i m))
@@ -83,6 +98,6 @@
   (doseq [a-len (range 25), b-len (range 25)]
     (let [a-s (map #(symbol (str % 'a)) (range a-len))
           b-s (map #(symbol (str % 'b)) (range b-len))
-          a (reduce conjr nil a-s)
-          b (reduce conjr nil b-s)]
+          a (apply finger-tree nil a-s)
+          b (apply finger-tree nil b-s)]
       (is (= (seq (concat a-s b-s)) (ft-seq (ft-concat a b)))))))
